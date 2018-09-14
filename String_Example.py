38e0eec34de04d1a42d39974bcf09270a50c2626
Jack Zhu <Jack.zhu@0100101001011010.local>
2018-09-14 09:57:24 -0400

    message

---
+++ String_Example.py
@@ -0,0 +1,26 @@
+String_Example.py
+
+#Strings are collections of characters
+#They are incased in ""
+#2 things we need to talk about
+#When we think of strings
+#index - always starts at 0
+# length
+#Example:
+# 0123 012345
+# "Paul" "Monkey"
+#len("Paul") = 4
+#len("Monkey") = 6
+
+name = "Jack"
+print(name)
+sentance = name + "is cool!"
+print (sentance)
+fLetter = name[0]
+print (fLetter)
+letters1 = name[0:2]#inclusive and exclusive
+print(letters1)
+letters2 = name[2:]
+print (letters2)
+letters3 = name[:2]
+print(letters3)
--- TakingInputs.py
+++ TakingInputs.py
@@ -1,3 +1,7 @@
 r = input("Input radius: ")
+r = int(r)
 h = input("Input height; ")
+h = int(h)
 
+sa = 2.0*3.14*r*r + 2.0*r*h*3.14
+print(sa)
