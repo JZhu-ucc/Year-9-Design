02524c726b42e031834a5860e7b5972ffdb7a38e
Jack Zhu <Jack.zhu@0100101001011010.local>
2018-09-26 10:28:50 -0400

    miskew push

---
+++ CylinderCalculator.py
@@ -0,0 +1,25 @@
+import tkinter as tk
+
+root = tk.Tk() 
+root.title("Volume of a cylinder")
+
+labr = tk.Label(root, text="radius")
+labr.pack()
+entr = tk.Entry(root)
+entr.pack()
+
+labh = tk.Label(root, text="Height")
+labh.pack()
+
+enth = tk.Entry(root)
+enth.pack()
+
+btn = tk.Button(root, text="Submit")
+btn.pack()
+
+output = tk.Text(root, width = 50, height = 10, borderwidth = 3, relief =tk.GROOVE)
+output.pack()
+
+
+
+root.mainloop()
