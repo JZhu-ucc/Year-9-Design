dd76a6b87693c9195cc188e5b804bcff285b482b
Jack Zhu <Jack.zhu@0100101001011010.local>
2018-10-02 12:10:20 -0400

    hi

---
+++ Loops2.py
@@ -0,0 +1,16 @@
+print ("**********************************************")
+print("Counted loops: For ")
+
+print("0")
+print("2")
+print("3")
+print("4")
+print("5")
+print("6")
+print("7")
+
+
+for i in range(0, 4, 5):
+	print(i)
+
+	
